bf69eeebc303befa251f6396cf3e94f25c5dc1f9
juissi999 <jussi.kaatiala@gmail.com>
2018-08-08 17:03:12 +0300

    More features added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# code_analysis
-A library for python code analysis
+# coanlib
+A library for python code analysis.
+
+Coanlib gives the user tools to list all the functions and function calls of a
+source code file. It facilitates the task to find unused functions, etc.
--- coanlib.py
+++ coanlib.py
@@ -13,29 +13,74 @@ def functions(lines):
 
     functions = []
 
-    for l in lines:
+    lines_nocom = remove_comments(lines)
 
-        fs = l
-        # remove comments
-        re.sub("#.*", "", fs)
+    for l in lines_nocom:
 
-        if re.match(" *def ", fs) is not None:
+        if re.match(" *def ", l) is not None:
 
             # remove space before def and after
-            fs = re.sub(" *def ", "", fs)
+            l = re.sub(" *def ", "", l)
 
             # remove double-comma and things after that and EOL
             # assume no double-comma used in function definition
-            fs = re.sub(":.*\n", "", fs)
-            functions.append(fs)
+            l = re.sub(":.*\n", "", l)
+            functions.append(l)
 
     return functions
 
 def remove_comments(codelines):
 
-    for l in lines:
+    lines_removed = []
+    for l in codelines:
 
         # remove comments
-        re.sub("#.*", "", l)
+        lines_removed.append(re.sub("#.*", "", l))
 
-    return l
+    return lines_removed
+
+def concatenate_codelines(codelines):
+
+    codeline = ""
+
+    for l in codelines:
+        codeline = codeline + l
+
+    return codeline
+
+def calculate_count(expr, codelines):
+
+    count = 0
+    for l in codelines:
+        if re.match(expr, l) is not None:
+            count +=1
+
+    return count
+
+def remove_strings(codelines):
+
+    reg = re.compile("\"[^\"]*\"")
+    
+    newlines = []
+    for l in codelines:
+        l = re.sub("\"[^\"]*\"", "", l)
+        newlines.append(l)
+
+    return newlines
+
+def code_statistics(filepath):
+
+    c = read_codelines(filepath)
+
+    c_stripped = remove_strings(c)
+    commentlinec = calculate_count(" *#", c_stripped)
+    trailing_commentc = calculate_count("(.*)#", c_stripped) - commentlinec
+    fcs = functions(c)
+
+    print("Source code contains %d commentlines" % commentlinec)
+    print("Source code contains %d trailing comments" % trailing_commentc)
+    print("Source code contains %d functions" % len(fcs))
+    print("Functions listed:")
+
+    for i in fcs:
+        print(" " + i)
